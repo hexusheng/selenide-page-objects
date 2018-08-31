64b2f7ff7a35afb845b47f35724eb3f0a2be8199
Andreas Kaspari <andreas.kaspari@gmail.com>
2018-08-31 21:36:15 +1200

    More idea around collections

--- src/main/java/com/github/aski/selenide/PageModuleCollection.java
+++ src/main/java/com/github/aski/selenide/PageModuleCollection.java
@@ -9,7 +9,7 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.util.function.Supplier;
 
-public class PageModuleCollection<T extends AbstractPageModule> {
+public final class PageModuleCollection<T extends AbstractPageModule> {
     private final Supplier<ElementsCollection> selector;
     private final Class<T> pageModuleClass;
 
@@ -26,6 +26,30 @@ public class PageModuleCollection<T extends AbstractPageModule> {
         selector.get().shouldHave(conditions);
     }
 
+    public void shouldAll(Condition condition) {
+        selector.get().excludeWith(condition).shouldHaveSize(0);
+    }
+
+    public void shouldAllBe(Condition condition) {
+        shouldAll(condition);
+    }
+
+    public void shouldAllHave(Condition condition) {
+        shouldAll(condition);
+    }
+
+    public void shouldAllNot(Condition condition) {
+        selector.get().filterBy(condition).shouldHaveSize(0);
+    }
+
+    public void shouldAllNotBe(Condition condition) {
+        shouldAllNot(condition);
+    }
+
+    public void shouldAllNotHave(Condition condition) {
+        shouldAllNot(condition);
+    }
+
     public void shouldHaveSize(int size) {
         selector.get().shouldHaveSize(size);
     }
@@ -34,11 +58,34 @@ public class PageModuleCollection<T extends AbstractPageModule> {
         return new PageModuleCollection<>(() -> selector.get().filterBy(condition), pageModuleClass);
     }
 
-    public T findBy(Condition condition) {
-        Supplier<SelenideElement> element = () -> selector.get().findBy(condition);
+    public PageModuleCollection<T> excludeWith(Condition condition) {
+        return new PageModuleCollection<>(() -> selector.get().excludeWith(condition), pageModuleClass);
+    }
+
+    public T first() {
+        return makePageModule(() -> selector.get().first());
+    }
+
+    public PageModuleCollection<T> first(int elements) {
+        return new PageModuleCollection<>(() -> selector.get().first(elements), pageModuleClass);
+    }
+
+    public T last() {
+        return makePageModule(() -> selector.get().last());
+    }
+
+    public PageModuleCollection<T> last(int elements) {
+        return new PageModuleCollection<>(() -> selector.get().last(elements), pageModuleClass);
+    }
+
+    public T findBy(final Condition condition) {
+        return makePageModule(() -> selector.get().findBy(condition));
+    }
+
+    private T makePageModule(Supplier<SelenideElement> supplier) {
         try {
             Constructor<T> constructor = pageModuleClass.getConstructor(Supplier.class);
-            return constructor.newInstance((Supplier) () -> selector.get().findBy(condition));
+            return constructor.newInstance(supplier);
         } catch (NoSuchMethodException | InstantiationException | IllegalAccessException | InvocationTargetException e) {
             throw new RuntimeException(e);
         }
--- src/test/java/com/github/aski/selenide/CollectionsTest.java
+++ src/test/java/com/github/aski/selenide/CollectionsTest.java
@@ -3,7 +3,6 @@ package com.github.aski.selenide;
 import com.github.aski.selenide.modules.TextField;
 import org.openqa.selenium.By;
 
-import static com.codeborne.selenide.CollectionCondition.empty;
 import static com.codeborne.selenide.CollectionCondition.texts;
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selenide.$$;
@@ -12,23 +11,27 @@ public class CollectionsTest {
 
     public void method() {
         // create a collection for all web elements with class 'foo'
-        PageModuleCollection<TextField> allTextFields =
+        PageModuleCollection<TextField> textFields =
                 new PageModuleCollection<>(() -> $$(By.className("foo")), TextField.class);
 
         // assert that there are 5 of them
-        allTextFields.shouldHaveSize(5);
+        textFields.shouldHaveSize(5);
 
-        // assert that all text fields are editable
-        allTextFields.filterBy(readonly).shouldBe(empty);
+        textFields.shouldAll(appear);
+
+        // assert that all text fields are enabled - can text fields be enabled ?
+        textFields.shouldAllBe(enabled);
 
         // or another way to check the same:
-        allTextFields.filterBy(not(readonly)).shouldHaveSize(5);
+        textFields.filterBy(not(readonly)).shouldHaveSize(5);
 
         // the first text field should have '1', ...
-        allTextFields.shouldHave(texts("1", "2", "3", "4", "5"));
+        textFields.shouldHave(texts("1", "2", "3", "4", "5"));
 
         // the text field with the '1' in it should be focused
-        allTextFields.findBy(text("1")).shouldBe(focused);
+        textFields.findBy(text("1")).shouldBe(focused);
 
+        // the rest of the text fields should all be hidden
+        textFields.last(4).shouldAllNotBe(visible);
     }
 }
